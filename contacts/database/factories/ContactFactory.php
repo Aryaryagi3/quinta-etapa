7a77775460782aa103c6fa2e86f52bbc182c35f5
Lets <lets@pop-os.localdomain>
2022-05-06 09:10:15 -0300

    Primeiro commit

---
+++ contacts/database/factories/ContactFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Contact;
+
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'phone' => $this->faker->numerify('#########'),
+             'email' => $this->faker->email,
+        ];
+    }
+}

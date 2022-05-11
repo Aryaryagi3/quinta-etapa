833eb0574ac9f6b459320991e33087c0b5a8d828
Alexandre Takeshi Ogassahara gonzaga <86446096+Aryaryagi3@users.noreply.github.com>
2022-05-11 14:50:34 -0300

    Add files via upload

---
+++ contacts/resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.main')
+
+@section('title', 'Contatos')
+
+@section('content')
+<div class="px-4 py-4 my-5 text-center">
+    <i class="fa-solid fa-phone fa-5x"></i>
+    <br>
+    <br>
+    <h1 class="display-5 fw-bold">Contatos</h1>
+    <div class="col-lg-6 mx-auto">
+    <p class="lead mb-4">Seja bem vindo, registre seus contatos aqui de forma rápida e fácil.</p>
+    <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <a href="/register"><button type="button" class="btn btn-primary btn-lg px-4 gap-3">Criar conta</button></a>
+        <a href="/login"><button type="button" class="btn btn-secondary btn-lg px-4 gap-3">Entrar</button></a>
+    </div>
+    </div>
+</div>
+@endsection

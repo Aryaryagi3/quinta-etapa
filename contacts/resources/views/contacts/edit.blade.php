b8d4e21d753a0e3c095580623dc3763e451caf15
Alexandre Takeshi Ogassahara gonzaga <86446096+Aryaryagi3@users.noreply.github.com>
2022-05-12 09:16:42 -0300

    Add files via upload

--- contacts/resources/views/contacts/edit.blade.php
+++ contacts/resources/views/contacts/edit.blade.php
@@ -10,21 +10,24 @@
             {{ method_field('PATCH') }}
             {{ csrf_field()}}
 
-            <div class="form-group">
-                <label for="name">Nome do contato: </label>
-                <input type="text" name="name" value="{{$contact->name}}" class="form-control">
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingInput" name="name" placeholder="Insira o nome aqui" value="{{$contact->name}}">
+                <label for="floatingInput">Nome do contato</label>
+                <p class="text-secondary"><small>Formato: exemplo exemplo</small></p>
             </div>
 
-            <div class="form-group">
-                <label for="phone">Telefone do contato: </label>
-                <input type="text" name="phone" value="{{$contact->phone}}" class="form-control">
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" name="email" placeholder="Insira o e-mail aqui" value="{{$contact->email}}">
+                <label for="floatingInput">E-mail</label>
+                <p class="text-secondary"><small>Formato: exemplo@exemplo</small></p>
             </div>
 
-            <div class="form-group">
-                <label for="email">Email do contato: </label>
-                <input type="text" name="email" value="{{$contact->email}}" class="form-control">
+            <div class="form-floating mb-3">
+                <input type="tel" pattern="[0-9]{9}" class="form-control" id="floatingInput" name="phone" placeholder="Insira o telefone aqui" value="{{$contact->phone}}">
+                <label for="floatingInput">Telefone</label>
+                <p class="text-secondary"><small>Formato: xxxxxxxxx</small></p>
             </div>
-
+            <br>
             <div>
                 <button class="btn btn-primary" type="submit">Atualizar contato</button>
             </div>
@@ -35,4 +38,5 @@
             {{ csrf_field()}}
             <button class="btn btn-primary btn-danger" type="submit">Apagar contato</button>
         </form>
+        <br>
 @endsection
